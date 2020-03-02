6c91706ba8d7213b4a0021c44969131a17c5b753
Benny~ <benny.think@gmail.com>
2020-03-02 21:50:04 +0800

    add build scripts

---
+++ scripts/unix.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+cd ..
+git pull
+platform=$(uname -a)
+
+if [[ $platform =~ "Darwin" ]]
+then
+    go build -o webp-server-darwin-amd64 webp-server.go
+elif [[ $platform =~ "x86_64" ]];then
+    go build -o webp-server-unix-amd64 webp-server.go
+else
+  go build -o webp-server-linux-i386 webp-server.go
+fi
