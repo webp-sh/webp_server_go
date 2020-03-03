95da3e81a4ebeb999c2770142a9825716366a766
n0vad3v <n0vad3v@riseup.net>
2020-03-03 21:23:06 +0800

    Adjust path

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,19 +4,19 @@ CGO_ENABLED=0
 
 GOOS=linux
 GOARCH=amd64
-go build -x -v -ldflags "-s -w" -o builds/webp-server-linux-amd64 ../webp-server.go
+go build -x -v -ldflags "-s -w" -o builds/webp-server-linux-amd64
 
 GOOS=linux
 GOARCH=arm
-go build -x -v -ldflags "-s -w" -o builds/webp-server-linux-arm ../webp-server.go
+go build -x -v -ldflags "-s -w" -o builds/webp-server-linux-arm
 
 GOOS=darwin
 GOARCH=amd64
-go build -x -v -ldflags "-s -w" -o builds/webp-server-darwin-amd64 ../webp-server.go
+go build -x -v -ldflags "-s -w" -o builds/webp-server-darwin-amd64
 
 GOOS=windows
 GOARCH=amd64
-go build -x -v -ldflags "-s -w" -o builds/webp-server-windows-amd64.exe ../webp-server.go
+go build -x -v -ldflags "-s -w" -o builds/webp-server-windows-amd64.exe
 
 echo "build done!"
-ls builds
+ls
