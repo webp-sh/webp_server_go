32f50c4306f331012b8b2186dcf128a3fe85fb77
n0vad3v <n0vad3v@riseup.net>
2020-03-03 22:41:04 +0800

    Correct filename

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,12 @@
 
 CGO_ENABLED=0
 
-go build -v -ldflags "-s -w" -o builds/webp-server-linux-${1}
+if [ "${1}" == "windows" ]
+then
+    go build -v -ldflags "-s -w" -o builds/webp-server-${1}-${2}.exe
+else
+    go build -v -ldflags "-s -w" -o builds/webp-server-${1}-${2}
+fi
 
 echo "build done!"
 ls builds
