e399045bc76124a63901dcd96d9c352aa043d6e0
n0vad3v <n0vad3v@riseup.net>
2020-03-03 22:10:25 +0800

    Compile for multiple branch

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,21 +2,7 @@
 
 CGO_ENABLED=0
 
-GOOS=linux
-GOARCH=amd64
-go build -x -v -ldflags "-s -w" -o builds/webp-server-linux-amd64
-
-GOOS=linux
-GOARCH=arm
-go build -x -v -ldflags "-s -w" -o builds/webp-server-linux-arm
-
-GOOS=darwin
-GOARCH=amd64
-go build -x -v -ldflags "-s -w" -o builds/webp-server-darwin-amd64
-
-GOOS=windows
-GOARCH=amd64
-go build -x -v -ldflags "-s -w" -o builds/webp-server-windows-amd64.exe
+go build -x -v -ldflags "-s -w" -o builds/webp-server-linux-${1}
 
 echo "build done!"
 ls builds
