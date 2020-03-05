2c7b87c1f24bf356fc80b1e5caec895b2d86f11b
n0vad3v <n0vad3v@riseup.net>
2020-03-05 21:16:48 +0800

    fix: build script naming problem on osx

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,6 +5,9 @@ CGO_ENABLED=0
 if [ "${1}" == "windows" ]
 then
     go build -v -ldflags "-s -w" -o builds/webp-server-${1}-${2}.exe
+elif [ "${1}" == "osx" ]
+then
+    go build -v -ldflags "-s -w" -o builds/webp-server-darwin-${2}
 else
     go build -v -ldflags "-s -w" -o builds/webp-server-${1}-${2}
 fi
