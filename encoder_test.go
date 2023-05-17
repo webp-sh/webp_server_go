998afccd2646fd36fed303dd7bc163dbfbff020a
n0vad3v <n0vad3v@riseup.net>
2023-05-17 19:43:50 +0800

    Add TestResizeImage

--- encoder_test.go
+++ encoder_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/davidbyttow/govips/v2/vips"
 	"github.com/stretchr/testify/assert"
 
 	"os"
@@ -21,6 +22,69 @@ func walker() []string {
 		return nil
 	})
 	return list
+}
+
+func TestResizeImage(t *testing.T) {
+	// Create a test image with specific dimensions
+	testImage, _ := vips.NewImageFromFile("./pics/png.jpg")
+
+	// Test case 1: Both width and height are greater than 0
+	params1 := ExtraParams{
+		Width:  100,
+		Height: 100,
+	}
+	err := resizeImage(testImage, params1)
+	if err != nil {
+		t.Errorf("Error occurred while resizing image: %v", err)
+	}
+
+	// Assert the resized image has the expected dimensions
+	resizedWidth1 := testImage.Width()
+	resizedHeight1 := testImage.Height()
+	expectedWidth1 := params1.Width
+	expectedHeight1 := params1.Height
+	// If both width and height are provided, follow Width and keep aspect ratio
+	if resizedWidth1 != expectedWidth1 {
+		t.Errorf("Resized image dimensions do not match. Expected: %dx%d, Actual: %dx%d",
+			expectedWidth1, expectedHeight1, resizedWidth1, resizedHeight1)
+	}
+
+	// Test case 2: Only width is greater than 0
+	params2 := ExtraParams{
+		Width:  100,
+		Height: 0,
+	}
+	err = resizeImage(testImage, params2)
+	if err != nil {
+		t.Errorf("Error occurred while resizing image: %v", err)
+	}
+
+	// Assert the resized image has the expected width
+	resizedWidth2 := testImage.Width()
+	expectedWidth2 := params2.Width
+	if resizedWidth2 != expectedWidth2 {
+		t.Errorf("Resized image width does not match. Expected: %d, Actual: %d",
+			expectedWidth2, resizedWidth2)
+	}
+
+	// Test case 3: Only height is greater than 0
+	params3 := ExtraParams{
+		Width:  0,
+		Height: 100,
+	}
+	err = resizeImage(testImage, params3)
+	if err != nil {
+		t.Errorf("Error occurred while resizing image: %v", err)
+	}
+
+	// Assert the resized image has the expected height
+	resizedHeight3 := testImage.Height()
+	expectedHeight3 := params3.Height
+	if resizedHeight3 != expectedHeight3 {
+		t.Errorf("Resized image height does not match. Expected: %d, Actual: %d",
+			expectedHeight3, resizedHeight3)
+	}
+
 }
 
 func TestWebPEncoder(t *testing.T) {
