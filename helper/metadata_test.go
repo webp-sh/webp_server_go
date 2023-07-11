fedd6700dc24d3583f4230754a1b10a23d786c37
Benny <benny.think@gmail.com>
2023-07-11 19:08:05 +0200

    use t.Run to get test case

--- helper/metadata_test.go
+++ helper/metadata_test.go
@@ -8,32 +8,36 @@ import (
 )
 
 func TestGetId(t *testing.T) {
-	// Test case 1: Proxy mode
-	config.ProxyMode = true
 	p := "https://example.com/image.jpg?width=200&height=300"
-	id, jointPath, santizedPath := getId(p)
 
-	// Verify the return values
-	expectedId := HashString(p)
-	expectedPath := ""
-	expectedSantizedPath := ""
-	if id != expectedId || jointPath != expectedPath || santizedPath != expectedSantizedPath {
-		t.Errorf("Test case 1 failed: Expected (%s, %s, %s), but got (%s, %s, %s)",
-			expectedId, expectedPath, expectedSantizedPath, id, jointPath, santizedPath)
-	}
+	t.Run("proxy mode", func(t *testing.T) {
+		// Test case 1: Proxy mode
+		config.ProxyMode = true
+		id, jointPath, santizedPath := getId(p)
 
-	// Test case 2: Non-proxy mode
-	config.ProxyMode = false
-	p = "/image.jpg?width=400&height=500"
-	id, jointPath, santizedPath = getId(p)
+		// Verify the return values
+		expectedId := HashString(p)
+		expectedPath := ""
+		expectedSantizedPath := ""
+		if id != expectedId || jointPath != expectedPath || santizedPath != expectedSantizedPath {
+			t.Errorf("Test case 1 failed: Expected (%s, %s, %s), but got (%s, %s, %s)",
+				expectedId, expectedPath, expectedSantizedPath, id, jointPath, santizedPath)
+		}
+	})
+	t.Run("non-proxy mode", func(t *testing.T) {
+		// Test case 2: Non-proxy mode
+		config.ProxyMode = false
+		p = "/image.jpg?width=400&height=500"
+		id, jointPath, santizedPath := getId(p)
 
-	// Verify the return values
-	parsed, _ := url.Parse(p)
-	expectedId = HashString(parsed.Path + "?width=400&height=500")
-	expectedPath = path.Join(config.Config.ImgPath, parsed.Path)
-	expectedSantizedPath = parsed.Path + "?width=400&height=500"
-	if id != expectedId || jointPath != expectedPath || santizedPath != expectedSantizedPath {
-		t.Errorf("Test case 2 failed: Expected (%s, %s, %s), but got (%s, %s, %s)",
-			expectedId, expectedPath, expectedSantizedPath, id, jointPath, santizedPath)
-	}
+		// Verify the return values
+		parsed, _ := url.Parse(p)
+		expectedId := HashString(parsed.Path + "?width=400&height=500")
+		expectedPath := path.Join(config.Config.ImgPath, parsed.Path)
+		expectedSantizedPath := parsed.Path + "?width=400&height=500"
+		if id != expectedId || jointPath != expectedPath || santizedPath != expectedSantizedPath {
+			t.Errorf("Test case 2 failed: Expected (%s, %s, %s), but got (%s, %s, %s)",
+				expectedId, expectedPath, expectedSantizedPath, id, jointPath, santizedPath)
+		}
+	})
 }
