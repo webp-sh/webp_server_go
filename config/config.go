f7880ea7dedd216752a2c180d115a1ff1b3a52be
n0vad3v <n0vad3v@riseup.net>
2025-04-06 15:11:26 +0800

    bump to 0.13.5

--- config/config.go
+++ config/config.go
@@ -55,7 +55,7 @@ var (
 	PrefetchForeground  bool // Standalone prefetch, prefetch and exit
 	AllowNonImage       bool
 	Config              = NewWebPConfig()
-	Version             = "0.13.4"
+	Version             = "0.13.5"
 	WriteLock           = cache.New(5*time.Minute, 10*time.Minute)
 	ConvertLock         = cache.New(5*time.Minute, 10*time.Minute)
 	LocalHostAlias      = "local"
